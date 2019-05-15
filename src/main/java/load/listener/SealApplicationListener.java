f94d7bef7311f9715b5bed974e1f5f58d343bc4e
ordinaryload <xnch888@aliyun.com>
2019-05-15 21:44:34 +0800

    commit

--- src/main/java/load/listener/SealApplicationListener.java
+++ src/main/java/load/listener/SealApplicationListener.java
@@ -1,10 +1,15 @@
 package load.listener;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+
+import java.io.IOException;
 
 public class SealApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -13,6 +18,14 @@ public class SealApplicationListener implements ApplicationListener<ContextRefre
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         System.out.println("容器加载完成");
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+        @Override
+        public void serialize(Object arg0, JsonGenerator arg1, SerializerProvider arg2) throws IOException, JsonProcessingException {
+            arg1.writeString("");
+            }
+        });
+
+//        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 }
